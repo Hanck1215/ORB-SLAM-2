0270710918a036c427a714e0465822ce81c84a00
Hanck1215 <st11025212@gmail.com>
2025-03-31 02:57:29 +0800

    新增 myORB-SLAM2 共享庫的 CMake 配置，包含 ImagePyramid.cpp 檔案並設定相關編譯規則

---
+++ build/src/CMakeFiles/myORB-SLAM2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../lib/libmyORB-SLAM2.pdb"
+  "../../lib/libmyORB-SLAM2.so"
+  "CMakeFiles/myORB-SLAM2.dir/ImagePyramid.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myORB-SLAM2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

19d4779dc5f6d16c10596bf9f9c93ed79515a66a
Hanck1215 <st11025212@gmail.com>
2025-03-31 06:08:34 +0800

    更新 testImagePyramid_00 測試程式以支援命令列參數，並改善影像顯示邏輯

--- test/testImagePyramid_00.cpp
+++ test/testImagePyramid_00.cpp
@@ -1,20 +1,33 @@
-#include "myORB-SLAM2/ImagePyramid.h"
-#include <opencv2/opencv.hpp>
+#include "myORB-SLAM2/ImagePyramid.h" // 引入自定義的 ImagePyramid 類別
+#include <opencv2/opencv.hpp> // 引入 OpenCV 的核心功能和影像處理功能
 
-using namespace my_ORB_SLAM2;
-using namespace std;
-using namespace cv;
+using namespace my_ORB_SLAM2; // 使用自定義命名空間 my_ORB_SLAM2
+using namespace std; // 使用標準命名空間
+using namespace cv; // 使用 OpenCV 的命名空間
 
+// test cmd: ./testImagePyramid_00 ../../images_00/000000.png
+// 測試命令：執行程式並傳入影像路徑作為參數
 int main(int argc, char **argv) {
-    string path = argv[1];
-    Mat image = imread(path);
+    string path = argv[1]; // 從命令列參數中獲取影像路徑
+    Mat image = imread(path); // 使用 OpenCV 的 imread 函式讀取影像
 
+    // 建立影像金字塔物件，設定層數為 8，每層縮放係數為 1.2，待提取的總特徵點數量為 2000
     ImagePyramid imagePyramid(8, 1.2, 2000);
-    imagePyramid.setImage(image);
+    imagePyramid.setImage(image); // 將讀取的影像設置為影像金字塔的第一層，並生成其他層
 
+    // 獲取影像金字塔的所有層影像
     vector<Mat> images = imagePyramid.getImages();
-    for(Mat &image : images) { imshow("image", image); }
+
+    // 遍歷影像金字塔的每一層，並顯示影像
+    int level = 0;
+    for(Mat &image : images) { 
+        string name = "image"; // 設定視窗名稱的前綴
+        name.append(to_string(level++)); // 為每層影像添加層數後綴
+        imshow(name, image); // 使用 OpenCV 的 imshow 函式顯示影像
+    }
+
+    // 等待使用者按下任意鍵後關閉所有視窗
     waitKey(0);
     destroyAllWindows();
-    return 0;
+    return 0; // 程式執行成功
 }

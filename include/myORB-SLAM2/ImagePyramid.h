9d264fe7fa441cf2023a4c5ce625ad187d398c14
Hanck1215 <st11025212@gmail.com>
2025-03-31 01:55:38 +0800

    新增 mvInvScaleFactor 成員變數以儲存每一層影像恢復為第一層影像大小所需的縮放倍數

--- include/myORB-SLAM2/ImagePyramid.h
+++ include/myORB-SLAM2/ImagePyramid.h
@@ -25,8 +25,9 @@ class ImagePyramid {
         double mScaleFactor; // 每層之間的縮放係數
 
     private :
-        vector<int> mvnFeaturesPerLevel; // 儲存每一層影像中應提取的特徵點數
-        vector<float> mvScaleFactor; // 儲存每一層影像相較於第一層影像的縮小倍數
+        vector<int> mvnFeaturesPerLevel; // 儲存每一層影像中應提取的「特徵點數」
+        vector<float> mvScaleFactor; // 儲存每一層影像相較於第一層影像的「縮小倍數」
+        vector<float> mvInvScaleFactor; // 儲存每一層影像恢復為第一層影像大小所需的「縮放倍數」
 
 
 };

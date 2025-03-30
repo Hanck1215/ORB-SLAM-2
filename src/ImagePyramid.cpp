4240056e4da217a4a1bbf9615437b9e2c3863be8
Hanck1215 <st11025212@gmail.com>
2025-03-31 03:02:59 +0800

    調整 ImagePyramid 類別的建構函數，將縮放係數的類型改為 double 以提高精度

--- src/ImagePyramid.cpp
+++ src/ImagePyramid.cpp
@@ -7,7 +7,9 @@ namespace my_ORB_SLAM2 {
     @param[in] m 影像金字塔的層數
     @param[in] s 每層之間的縮放係數 (例如 1.2) 
     @param[in] N 總共需要提取的特徵點數量 */
-    ImagePyramid::ImagePyramid(int m, int s, int N) {
-
+    ImagePyramid::ImagePyramid(int m, double s, int N) {
+        mnLevels = m;
+        mnPoints = N;
+        mScaleFactor = s;
     }
 }

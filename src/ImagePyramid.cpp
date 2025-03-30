42ea3f989fdb40999e625aa6197c35bec8c9c469
Hanck1215 <st11025212@gmail.com>
2025-03-31 03:01:33 +0800

    新增 ImagePyramid 類別的建構函數以設定影像金字塔參數

---
+++ src/ImagePyramid.cpp
@@ -0,0 +1,13 @@
+#include "include/myORB-SLAM2/ImagePyramid.h"
+
+namespace my_ORB_SLAM2 {
+    /*
+    @brief 設定影像金字塔參數
+    
+    @param[in] m 影像金字塔的層數
+    @param[in] s 每層之間的縮放係數 (例如 1.2) 
+    @param[in] N 總共需要提取的特徵點數量 */
+    ImagePyramid::ImagePyramid(int m, int s, int N) {
+
+    }
+}

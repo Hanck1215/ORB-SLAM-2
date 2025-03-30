a813f78a48b6f40ca1d6b7e397d41e4966dc66e7
Hanck1215 <st11025212@gmail.com>
2025-03-31 04:59:38 +0800

    新增測試目標 testImagePyramid_00 以驗證影像金字塔功能，並更新相關的 CMake 檔案

---
+++ build/test/CMakeFiles/testImagePyramid_00.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../bin/testImagePyramid_00"
+  "../../bin/testImagePyramid_00.pdb"
+  "CMakeFiles/testImagePyramid_00.dir/testImagePyramid_00.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testImagePyramid_00.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/test/CMakeFiles/testImagePyramid_00.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for testImagePyramid_00.
+# This may be replaced when dependencies are built.

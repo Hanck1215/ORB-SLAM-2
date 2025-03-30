bd81fc64457446ca7809f50ab674d6c1ddd2540b
Hanck1215 <st11025212@gmail.com>
2025-03-31 01:22:09 +0800

    新增 ImagePyramid 類別，移除 ORBExtractor 類別，更新 CMake 配置檔案

---
+++ include/myORB-SLAM2/ImagePyramid.h
@@ -0,0 +1,20 @@
+#ifndef IMAGEPYRAMID_H
+#define IMAGEPYRAMID_H
+
+#include <vector>
+#include <list>
+#include <opencv/cv.h>
+
+using namespace cv;
+using namespace std;
+
+namespace my_ORB_SLAM2 {
+
+class ImagePyramid {
+    public :
+    ImagePyramid ();
+    ~ImagePyramid () {};
+};
+
+}
+#endif

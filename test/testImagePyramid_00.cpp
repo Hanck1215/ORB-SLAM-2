b9a0d7d65fadc832b9dad78f4de653c29610df13
Hanck1215 <st11025212@gmail.com>
2025-03-31 06:16:55 +0800

    在 testImagePyramid_00 測試程式中新增計時功能，以測量影像金字塔參數設置和影像設置的執行時間

--- test/testImagePyramid_00.cpp
+++ test/testImagePyramid_00.cpp
@@ -1,5 +1,6 @@
 #include "myORB-SLAM2/ImagePyramid.h" // 引入自定義的 ImagePyramid 類別
 #include <opencv2/opencv.hpp> // 引入 OpenCV 的核心功能和影像處理功能
+#include <chrono>
 
 using namespace my_ORB_SLAM2; // 使用自定義命名空間 my_ORB_SLAM2
 using namespace std; // 使用標準命名空間
@@ -12,8 +13,19 @@ int main(int argc, char **argv) {
     Mat image = imread(path); // 使用 OpenCV 的 imread 函式讀取影像
 
     // 建立影像金字塔物件，設定層數為 8，每層縮放係數為 1.2，待提取的總特徵點數量為 2000
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     ImagePyramid imagePyramid(8, 1.2, 2000);
-    imagePyramid.setImage(image); // 將讀取的影像設置為影像金字塔的第一層，並生成其他層
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time_used = 
+    std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    printf("\nSetting Parameters costs: %f\n", time_used.count());
+
+    // 將讀取的影像設置為影像金字塔的第一層，並生成其他層
+    t1 = std::chrono::steady_clock::now();
+    imagePyramid.setImage(image);
+    t2 = std::chrono::steady_clock::now();
+    time_used = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    printf("Setting Images costs: %f\n", time_used.count());
 
     // 獲取影像金字塔的所有層影像
     vector<Mat> images = imagePyramid.getImages();

6ff8d351585508b64d3bab7aff8bd277cf971be6
hanck1215 <st11025212@gmail.com>
2025-09-25 17:59:37 +0800

    新增 keyPointDistributorPyramid 測試檔，從影像金字塔、關鍵點提取、再到關鍵點均勻化，並測試均勻化速度，在我的電腦中執行完一次關鍵點均勻化大概是 0.8 ms 上下

--- test/testKeyPointDistributorPyramid_00.cpp
+++ test/testKeyPointDistributorPyramid_00.cpp
@@ -13,42 +13,60 @@ int main(int argc, char **argv) {
     // 讀取指定影像
     string path = argv[1];
     Mat image = imread(path, 0);
-    Mat RGBImage = imread(path);
 
-    // 宣告影像金字塔
+    // 設定影像金字塔、關鍵點提取器
     ImagePyramid imagePyramid(3, 1.2, 2000);
-
-    // 宣告關鍵點提取器
-    vector<vector<KeyPoint>> keyPointsPerLavel;
-    KeyPointExtractorPyramid keyPointExtractorPyramid(imagePyramid.getLevels(), 30.0f, 19, imagePyramid.getPoints(), 20, 7);
+    vector<vector<KeyPoint>> vvKeyPointsPerLevel;
+    KeyPointExtractorPyramid keyPointExtractorPyramid(imagePyramid.mnLevels, 30.0f, 19, imagePyramid.mnPoints, 20, 7);
     KeyPointDistributorPyramid keyPointDistributorPyramid;
     
     // 設定影像到影像金字塔
     imagePyramid.setImage(image);
-    const vector<Mat> &images = imagePyramid.getImages();
+    vector<Mat> &vImagePerLevel = imagePyramid.mvImages;
 
     // 提取關鍵點
-    keyPointExtractorPyramid.extract(keyPointsPerLavel, images);
+    keyPointExtractorPyramid.extract(vvKeyPointsPerLevel, vImagePerLevel);
+
+    // 計算關鍵點均勻化所花費的時間
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 
     // 關鍵點均勻化
-    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-    vector<vector<KeyPoint>> resultKeyPointsPerLevel;
-    keyPointDistributorPyramid.distribute(imagePyramid.getFeaturesPerLevel(), resultKeyPointsPerLevel, keyPointsPerLavel, images);
+    vector<vector<KeyPoint>> vvDistributedKeyPointsPerLevel;
+    keyPointDistributorPyramid.distribute(
+        vvDistributedKeyPointsPerLevel,
+        vvKeyPointsPerLevel,
+        imagePyramid.mvnFeaturesPerLevel,
+        vImagePerLevel
+    );
+
+    printf("\n\nDistributed Key Points Information:\n");
+    for(int iLevel = 0; iLevel < vvDistributedKeyPointsPerLevel.size(); ++iLevel) {
+        printf(" - Level %d, key points size %d\n", iLevel, (int)vvDistributedKeyPointsPerLevel[iLevel].size());
+    }
      
     std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
     std::chrono::duration<double> time_used = 
     std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-    printf("\nkeyPointDistributorPyramid costs: %f\n", time_used.count());
+    printf("\nKey points distribution costs: %f\n", time_used.count());
 
-    for (const auto& kv : resultKeyPointsPerLevel) {
-        printf("%d keypoints\n", (int)kv.size());
+    // 顯示影像金字塔中，每一層的影像
+    int level = 0;
+    for(Mat &image : imagePyramid.mvImages) {
+        cv::Mat outImage;
+        cv::drawKeypoints(image, vvKeyPointsPerLevel[level++], outImage, cv::Scalar(0,255,0), cv::DrawMatchesFlags::DEFAULT);
+        string windowName = "All Key Points";
+        windowName.append(to_string(level));
+        cv::imshow(windowName, outImage);
     }
 
-    // 比較兩者差異
-    cv::Mat outImg;
-    
-    cv::drawKeypoints(RGBImage, resultKeyPointsPerLevel[0], outImg, cv::Scalar(0,255,0), cv::DrawMatchesFlags::DEFAULT);
-    cv::imshow("KeyPoints", outImg);
+    level = 0;
+    for(Mat &image : imagePyramid.mvImages) {
+        cv::Mat outImage;
+        cv::drawKeypoints(image, vvDistributedKeyPointsPerLevel[level++], outImage, cv::Scalar(0,255,0), cv::DrawMatchesFlags::DEFAULT);
+        string windowName = "Distributed Key Points";
+        windowName.append(to_string(level));
+        cv::imshow(windowName, outImage);
+    }
 
     char key = '\0';
     while(key != 'q') {
